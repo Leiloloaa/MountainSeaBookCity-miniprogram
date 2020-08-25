cb4a646e16cc2e45e99896cc51269a0c7798e6b2
cl1997 <leiloloaa@gmail.com>
2020-08-25 16:13:31 +0800

    新增购物车页面

--- mini-stone-shop/pages/cart/cart.js
+++ mini-stone-shop/pages/cart/cart.js
@@ -1,23 +1,29 @@
 // pages/cart.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+    /**
+     * 组件的属性列表
+     */
+    properties: {
 
-  },
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        value1: 1,
+    },
 
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        handleChange1({
+            detail
+        }) {
+            this.setData({
+                value1: detail.value
+            })
+        },
+    }
 })
